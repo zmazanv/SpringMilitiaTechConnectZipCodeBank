2fd96b51a90892109a5aceb46b5161bc755aa737
Sebastián Zavala Villagómez <zmazanv@proton.me>
2023-06-08 19:17:28 -0400

    Add getters and setters for Address

--- src/main/java/bank/connect/tech/model/Address.java
+++ src/main/java/bank/connect/tech/model/Address.java
@@ -26,4 +26,52 @@ public class Address {
 
     @Column(name = "zip")
     private String zip;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet_number() {
+        return this.street_number;
+    }
+
+    public void setStreet_number(String street_number) {
+        this.street_number = street_number;
+    }
+
+    public String getStreet_name() {
+        return this.street_name;
+    }
+
+    public void setStreet_name(String street_name) {
+        this.street_name = street_name;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return this.zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
 }
