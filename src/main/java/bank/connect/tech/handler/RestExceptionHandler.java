ecc639ad4d6a3927d9636a193932988180f4fa81
zmazanv <95514881+zmazanv@users.noreply.github.com>
2023-06-10 15:49:57 -0400

    Update RestExceptionHandler.java

--- src/main/java/bank/connect/tech/handler/RestExceptionHandler.java
+++ src/main/java/bank/connect/tech/handler/RestExceptionHandler.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import javax.security.auth.login.AccountNotFoundException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
