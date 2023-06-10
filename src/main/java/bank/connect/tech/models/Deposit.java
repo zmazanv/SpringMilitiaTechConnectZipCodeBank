b116767bf6e8f90bb7e08c3584c463a1323dd5be
MMaldonado1017 <miguelmaldonado199@gmail.com>
2023-06-10 12:31:35 -0400

    Merge branch 'main' of https://github.com/zmazanv/SpringMilitiaTechConnectZipCodeBank into miguel

--- src/main/java/bank/connect/tech/models/Deposit.java
+++ src/main/java/bank/connect/tech/models/Deposit.java
@@ -2,9 +2,7 @@ package bank.connect.tech.models;
 
 public class Deposit {
     private Long id;
-    private TransactionType type;
-    private String transactionDate;
-    private DepositStatus status;
+
     private Long payeeId;
     private DepositMedium medium;
     private Double amount;
@@ -13,50 +11,19 @@ public class Deposit {
     public Deposit() {
     }
 
-    public Deposit(Long id, TransactionType type, String transactionDate, DepositStatus status, Long payeeId,
-                   DepositMedium medium, Double amount, String description) {
+    public Deposit(Long id, , Long payeeId, DepositMedium medium, Double amount, String description) {
         this.id = id;
-        this.type = type;
-        this.transactionDate = transactionDate;
-        this.status = status;
         this.payeeId = payeeId;
         this.medium = medium;
         this.amount = amount;
         this.description = description;
     }
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
-    public TransactionType getType() {
-        return type;
-    }
-
-    public void setType(TransactionType type) {
-        this.type = type;
-    }
-
-    public String getTransactionDate() {
-        return transactionDate;
-    }
-
-    public void setTransactionDate(String transactionDate) {
-        this.transactionDate = transactionDate;
-    }
-
-    public DepositStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(DepositStatus status) {
-        this.status = status;
-    }
-
     public Long getPayeeId() {
         return payeeId;
     }
