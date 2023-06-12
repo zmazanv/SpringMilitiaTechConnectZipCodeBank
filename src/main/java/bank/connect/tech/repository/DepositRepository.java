9192b9d29f25f6d1dcbde8ad3a382ec6a86019b8
Sebastián Zavala Villagómez <zmazanv@proton.me>
2023-06-12 00:37:39 -0400

    Update DepositRepository

--- src/main/java/bank/connect/tech/repository/DepositRepository.java
+++ src/main/java/bank/connect/tech/repository/DepositRepository.java
@@ -3,6 +3,4 @@ package bank.connect.tech.repository;
 import bank.connect.tech.model.Customer;
 import org.springframework.data.repository.CrudRepository;
 
-public interface DepositRepository extends CrudRepository<Customer, Long> {
-
-}
+public interface DepositRepository extends CrudRepository<Customer, Long> {}
