6829cfe69af2005f08a9d59f942148f682228c07
Sebastián Zavala Villagómez <zmazanv@proton.me>
2023-06-12 02:35:55 -0400

    Create NotFoundResponse and use in handler

---
+++ src/main/java/bank/connect/tech/response/exception/error/NotFoundResponse.java
@@ -0,0 +1,14 @@
+package bank.connect.tech.response.exception.error;
+
+public class NotFoundResponse {
+
+    private int code;
+    private String message;
+
+
+    public int getCode() {return this.code;}
+    public void setCode(int code) {this.code = code;}
+
+    public String getMessage() {return this.message;}
+    public void setMessage(String message) {this.message = message;}
+}
