65fb492173df8d5986f9ac8bea8c032c7f7dbfe6
ev3lasco <evelasco1423@gmail.com>
2023-06-11 23:45:39 -0400

    update

--- src/main/java/bank/connect/tech/model/enumeration/TransactionType.java
+++ src/main/java/bank/connect/tech/model/enumeration/TransactionType.java
@@ -1,7 +1,32 @@
 package bank.connect.tech.model.enumeration;
 
+import bank.connect.tech.response.exception.MissingPropertyException;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum TransactionType {
-    P2P,
-    DEPOSIT,
-    WITHDRAWAL
+    P2P ("P2P"),
+    DEPOSIT ("Deposit"),
+    WITHDRAWAL ("Recurring");
+
+    private final String status;
+
+    private TransactionType (String status){
+        this.status = status;
+
+    }
+
+    @Override
+    @JsonValue
+    public String toString() {
+        return this.status;
+    }
+
+    public static TransactionType fromString(String statusString) {
+        for (TransactionType status : TransactionType.values()) {
+            if (status.toString().equalsIgnoreCase(statusString)) {
+                return status;
+            }
+        }
+        throw (new MissingPropertyException("No enum constant " + TransactionType.class.getCanonicalName()));
+    }
 }
