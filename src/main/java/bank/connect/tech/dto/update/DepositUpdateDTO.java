2a41d4e3980fe633bd60588a99b7cd421b269672
Sebastián Zavala Villagómez <zmazanv@proton.me>
2023-06-12 00:34:00 -0400

    Create DepositUpdateDTO

---
+++ src/main/java/bank/connect/tech/dto/update/DepositUpdateDTO.java
@@ -0,0 +1,18 @@
+package bank.connect.tech.dto.update;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class DepositUpdateDTO {
+
+    @JsonProperty("status")
+    private String status;
+    @JsonProperty("description")
+    private String description;
+
+
+    public String getStatus() {return this.status;}
+    public void setStatus(String status) {this.status = status;}
+
+    public String getDescription() {return this.description;}
+    public void setDescription(String description) {this.description = description;}
+}
